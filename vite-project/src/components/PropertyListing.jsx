f579a1bdc436c598d3b19419fc2ef1d28d5acb08
grace ngure <wnguregrace@gmail.com>
2024-05-08 11:07:59 +0300

    feat:update new styles for fetched data

--- vite-project/src/components/PropertyListing.jsx
+++ vite-project/src/components/PropertyListing.jsx
@@ -1,30 +1,37 @@
 import React, { useEffect, useState } from "react";
+
 function PropertyListing() {
   const [properties, setProperties] = useState([]);
+
   useEffect(() => {
-    fetch('http://localhost:3000/property')
+    fetch("http://localhost:3000/property")
       .then((response) => response.json())
       .then((data) => setProperties(data));
   }, []);
-  
+
   return (
     <div>
       <div className="container">
         {properties.map((property) => (
           <div key={property.id} className="card">
-            <h2>{property.name}</h2>
+            <h2 className="property-name">{property.name}</h2>
+            <img
+              src={property.image}
+              alt={property.name}
+              className="property-img"
+            />
             <div className="property-details">
-              <img
-                src={property.image}
-                alt={property.name}
-                className="property-img"
-              />
               <p>{property.description} </p>
-              <p> Bedrooms: {property.bedrooms}</p>
-              <p>Price: ${property.price} </p>
-              <p> Location: {property.location}</p>
-            </div>
+              <p>
+                {property.bedrooms !== undefined ? "Bedroom" : "Floors"}:{" "}
+                {property.bedrooms !== undefined
+                  ? property.bedrooms
+                  : property.floor}
+              </p>
 
+              <p>Price: ${property.price} </p>
+              <p>Location: {property.location}</p>
+            </div>
             {/* Add more details as needed */}
           </div>
         ))}
@@ -32,4 +39,5 @@ function PropertyListing() {
     </div>
   );
 }
+
 export default PropertyListing;
