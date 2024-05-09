0abaaf20a8f77df64e244931fed528a500799392
Joy <kweyuayieko@gmail.com>
2024-05-09 14:38:19 +0300

    Feat:Added delete button

--- vite-project/src/components/PropertyListing.jsx
+++ vite-project/src/components/PropertyListing.jsx
@@ -1,12 +1,16 @@
-import React from 'react';
+import React from "react";
 
-function PropertyListing({ properties  }) {
+function PropertyListing({ properties, onDelete }) {
   if (!properties || properties.length === 0) {
     return <div>Loading ...</div>;
   }
 
-  const categories = ['Apartments', 'Villas', 'Condos', 'Commercials'];
-  
+  const categories = ["Apartments", "Villas", "Condos", "Commercials"];
+
+  const handleDelete = (id) => {
+    onDelete(id);
+  };
+
   return (
     <div>
       {categories.map((category) => (
@@ -18,13 +22,22 @@ function PropertyListing({ properties  }) {
               <div key={property.id} className="card">
                 <h2 className="property-name">{property.name}</h2>
                 <div className="property-details">
-                  <img src={property.image} alt={property.name} className="property-img" />
+                  <img
+                    src={property.image}
+                    alt={property.name}
+                    className="property-img"
+                  />
                   <p>{property.description}</p>
-                  <p>{property.bedrooms !== undefined ? 'Bedroom' : 'Floor'}: {property.bedrooms !== undefined ? property.bedrooms : property.floor}</p>
+                  <p>
+                    {property.bedrooms !== undefined ? "Bedroom" : "Floor"}:{" "}
+                    {property.bedrooms !== undefined
+                      ? property.bedrooms
+                      : property.floor}
+                  </p>
                   <p>Price: ${property.price}</p>
                   <p>Location: {property.location}</p>
-                 
-                
+                  <br />
+                  <button className="delete-btn" onClick={() => handleDelete(property.id)}>DELETE</button>
                 </div>
               </div>
             ))}
